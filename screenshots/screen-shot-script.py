7681cbffa9a7b256e30bdfedff9dbe1501b88393
Alex Ayerdi <aayerdi@u.northwestern.edu>
2015-04-25 15:22:50 -0500

    ignore log and png, fix script

--- screenshots/screen-shot-script.py
+++ screenshots/screen-shot-script.py
@@ -8,62 +8,59 @@ import pyscreenshot as ImageGrab
 from urlparse import urlparse
 import shutil
 
-global_counter = 0
 pre_pic = ImageGrab.grab()
 post_pic = ImageGrab.grab()
 
 def start(context, flow):
-	#context.outFile = open("/home/alex/Desktop/MobileSystems/DependecyGraphGenerator/screenshots/output.log", 'w')      	
 	context.lock = threading.RLock()	
 	context.lastTime = 0
+	context.stop_analysis = False
 	pre_pic = ImageGrab.grab();
 	pre_pic.save("outputImagePre.png", "PNG")
-	f = open("output.log", "w").close() #truncate
+	open("output.log", "w").close()
 
 ##we want each request to be 5 seconds apart from *each other*
 @concurrent
 def request(context, flow):
-	''' context is thread specific '''
 
-	## locking...
-	context.lock.acquire()
+	if context.stop_analysis == False:
+		## locking...
+		context.lock.acquire()
 
-	## critical section - set your current waitTime and next lastTime
-        waitTime = context.lastTime + 5
-	context.lastTime += 5
+		## critical section - set your current waitTime and next lastTime
+        	waitTime = context.lastTime + 5
+		context.lastTime += 5
 
-	## releasing lock
-	context.lock.release()
+		## releasing lock
+		context.lock.release()
 
-        time.sleep(waitTime)	
+        	time.sleep(waitTime)	
 
 #just print out the lengths of each response for now
 def response(context, flow):
 
-	# fullscreen screenshot, save it locally for now
-	post_pic = ImageGrab.grab()
-	post_pic.save("outputImagePost.png", "PNG")
+	if context.stop_analysis == False:
+		# fullscreen screenshot, save it locally for now
+		post_pic = ImageGrab.grab()
+		post_pic.save("outputImagePost.png", "PNG")
 
-        url = "%s://%s%s" % (flow.request.scheme,flow.request.host,flow.request.path)
-        o = urlparse(url)
-        url = "%s://%s%s" % (flow.request.scheme,o.netloc,o.path)
-        url = url.lower()
+       		url = "%s://%s%s" % (flow.request.scheme,flow.request.host,flow.request.path)
+       		o = urlparse(url)
+       		url = "%s://%s%s" % (flow.request.scheme,o.netloc,o.path)
+       		url = url.lower()
 		
-	f = open("output.log", "a")
+		f = open("output.log", "a")
 
-	picture_similar = calc_similar_by_path("outputImagePre.png", "outputImagePost.png")
-	info = '[SIMILAR:%f][REQUEST:%s]' % (picture_similar, str(url))
+		picture_similar = calc_similar_by_path("outputImagePre.png", "outputImagePost.png")
+		info = '[SIMILAR:%f][REQUEST:%s]' % (picture_similar, str(url))
 
-	f.write(info + '\n')
-	f.close()
+		f.write(info + '\n')
+		f.close()
 
-	shutil.copy("outputImagePost.png", "outputImagePre.png")
+		shutil.copy("outputImagePost.png", "outputImagePre.png")
 
-	if picture_similar <= 0.85:
-		cleanup_and_exit()
-
-def cleanup_and_exit():
-	sys.exit(0)
+		if picture_similar <= 0.85 and picture_similar > 0.75:
+			context.stop_analysis = True
 
 def make_regalur_image(img, size = (256, 256)):
     return img.resize(size).convert('RGB')
